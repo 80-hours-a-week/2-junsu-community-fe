a544ed421e0511db7737abc834466255e572d980
hahark-ops <hahark@naver.com>
2026-01-22 10:29:58 +0900

    refactor: 프로젝트 구조 재정비 (public, src 분리)
    - 정적 파일(html, css, js)을 public 디렉터리로 이동
    - 서버 코드(server.js)를 src 디렉터리로 이동
    - npm start 스크립트 추가 및 서버 파일 경로 수정

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const port = 3000;
 
 // 정적 파일(CSS, JS 등)을 제공하기 위한 설정 (나중에 필요함)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // 루트 경로 ('/')로 접속하면 index.html 파일을 보내줌
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.listen(port, () => {
